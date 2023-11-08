5ca9ce543ca3088d6b4fb645c45c26fc0ccdb636
TarikAguen <98916020+TarikAguen@users.noreply.github.com>
2023-11-08 13:53:49 +0100

    reverse route

--- number-arabic-roman/src/component/Convertisseur.jsx
+++ number-arabic-roman/src/component/Convertisseur.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 function Convertisseur() {
   const [chiffreArabe, setChiffreArabe] = useState('');
   const [chiffreRomain, setChiffreRomain] = useState('');
+  const [conversionInverse, setConversionInverse] = useState(false);
 
   const handleChange = (e) => {
     setChiffreArabe(e.target.value);
@@ -10,12 +11,14 @@ function Convertisseur() {
 
   const convertirChiffre = async () => {
     try {
-      const response = await fetch('https://arab-to-roman-114f70a02b4f.herokuapp.com/convert', {
+      const route = conversionInverse ? 'reverse' : 'convert';
+
+      const response = await fetch(`https://arab-to-roman-114f70a02b4f.herokuapp.com/${route}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ number: chiffreArabe }),
+        body: JSON.stringify(conversionInverse ? { roman: chiffreArabe } : { number: chiffreArabe }),
       });
 
       if (!response.ok) {
@@ -23,31 +26,41 @@ function Convertisseur() {
       }
 
       const data = await response.json();
-      const romanNumeral = data.roman;
+      const resultat = conversionInverse ? data.arabic : data.roman;
 
-      setChiffreRomain(romanNumeral);
+      if (conversionInverse) {
+        setChiffreRomain(resultat.toString());
+      } else {
+        setChiffreRomain(resultat);
+      }
     } catch (error) {
       console.error(error);
       setChiffreRomain('Erreur de conversion');
     }
   };
 
+  const toggleConversion = () => {
+    setConversionInverse(!conversionInverse);
+    setChiffreArabe('');
+    setChiffreRomain('');
+  };
+
   return (
     <div>
       <div className='container'>
-
-        <h1>Convertisseur Chiffre Arabe en Chiffre Romain</h1>
-        <label htmlFor="chiffreArabe">Entrez un chiffre entre 1 et 3999 :</label>
+        <h1>Convertisseur {conversionInverse ? 'Chiffre Romain en Chiffre Arabe' : 'Chiffre Arabe en Chiffre Romain'}</h1>
+        <label htmlFor="chiffreArabe">{conversionInverse ? 'Entrez un chiffre romain :' : 'Entrez un chiffre entre 1 et 3999 :'}</label>
         <input
-          type="number"
+          type="text"
           id="chiffreArabe"
-          placeholder="Entrez un chiffre arabe"
+          placeholder={conversionInverse ? 'Entrez un chiffre romain' : 'Entrez un chiffre arabe'}
           value={chiffreArabe}
           onChange={handleChange}
-          />
-        <button onClick={convertirChiffre}>Convertir</button>
-        <label htmlFor="chiffreRomain">Chiffre Romain correspondant :</label>
+        />
+        <button onClick={convertirChiffre}>{conversionInverse ? 'Convertir en Chiffre Arabe' : 'Convertir en Chiffre Romain'}</button>
+        <label htmlFor="chiffreRomain">{conversionInverse ? 'Chiffre Arabe correspondant :' : 'Chiffre Romain correspondant :'}</label>
         <input type="text" id="chiffreRomain" value={chiffreRomain} readOnly />
+        <button onClick={toggleConversion}>Inverse</button>
       </div>
     </div>
   );
